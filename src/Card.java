cd49ab17f74c7b16ab9382f8dea263621beffd39
Glomzzz <glom@skillw.com>
2024-10-18 12:21:19 +0800

    Init

---
+++ src/Card.java
@@ -0,0 +1,19 @@
+public abstract class Card {
+    public static int RED = 0;
+    public static int BLUE = 1;
+    public static int GREEN = 2;
+
+    /**
+     * Black
+     * Yellow
+     * Red
+     * Orange
+     * Light Blue
+     * Green
+     * Pink
+     * Brown
+     * Light Green
+     * Blue
+     */
+
+}
