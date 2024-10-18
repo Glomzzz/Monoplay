3711141fe3dd15347c4c50e97b25432b897b0632
CWZ_Morro <chungwinzun7765@gmail.com>
2024-10-18 18:47:56 +0800

    Added display class

---
+++ src/com/skillw/mono/Display.java
@@ -0,0 +1,10 @@
+package com.skillw.mono;
+
+public class Display {
+    public void DisplayHeader(){
+        System.out.println("======================================");
+        System.out.println("|         Welcome to Monopoly         ");
+        System.out.println("======================================");
+
+    }
+}
--- src/com/skillw/mono/Main.java
+++ src/com/skillw/mono/Main.java
@@ -45,9 +45,6 @@ package com.skillw.mono;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("======================================");
-        System.out.println("|         Welcome to Monopoly         ");
-        System.out.println("======================================");
         System.out.println();
         System.out.println("How many players are there?");
     }
